97098b1a0ae4d541d0bf80ec5e912e19738d4a6b
mbalbino1997 <rmassimo.balbino@gmail.com>
2024-12-03 23:32:46 +0100

    ADD

--- src/components/layout/main/Main.jsx
+++ src/components/layout/main/Main.jsx
@@ -1,32 +1,56 @@
-import style from "./main.module.css"
-import Card from "../../ui/card/Card.jsx"
-import posts from "../../../data/posts.js"
-import { useState } from "react"
+import style from "./main.module.css";
+import Card from "../../ui/card/Card.jsx";
+import posts from "../../../data/posts.js";
+import { useState } from "react";
 
 const initialFormData = {
     title: "",
     content: "",
     image: "",
     published: false,
-}
-export default function () {
-    const [postsArray, setPostsArray] = useState(posts)
+    tags: [false, false, false, false],
+};
+const langTags = ["html", "css", "js", "php"];
+
+export default function PostsManager() {
+    const [postsArray, setPostsArray] = useState(posts);
     const [formData, setFormData] = useState(initialFormData);
+
     function handleFormData(e) {
-        const value = e.target.type === "checkbox" ?
-            e.target.checked : e.target.value;
-        setFormData((formData) => ({
-            ...formData,
-            [e.target.name]: value,
-        }));
+        const value = e.target.type === "checkbox" ? e.target.checked : e.target.value;
+
+        if (e.target.name === "tags") {
+            const index = parseInt(e.target.dataset.index, 10);
+            setFormData((prevFormData) => ({
+                ...prevFormData,
+                tags: prevFormData.tags.map((tag, i) => (i === index ? value : tag)),
+            }));
+        } else {
+            setFormData((prevFormData) => ({
+                ...prevFormData,
+                [e.target.name]: value,
+            }));
+        }
     }
+
     function handleSubmit(e) {
         e.preventDefault();
-        setPostsArray((postsArray) => [...postsArray, { id: postsArray.length + 1, ...formData }])
+        const selectedTags = formData.tags
+            .map((isSelected, index) => (isSelected ? langTags[index] : null))
+            .filter(Boolean);
+
+        const newPost = {
+            id: postsArray.length + 1,
+            ...formData,
+            tags: selectedTags,
+        };
+
+        setPostsArray((prevPosts) => [...prevPosts, newPost]);
         setFormData(initialFormData);
     }
+
     function deletePost(id) {
-        const updatedPosts = postsArray.filter(post => post.id !== id)
+        const updatedPosts = postsArray.filter((post) => post.id !== id);
         setPostsArray(updatedPosts);
     }
 
@@ -36,43 +60,87 @@ export default function () {
                 <div className={style.col_12}>
                     <form onSubmit={handleSubmit}>
                         <div>
-                            <label htmlFor="title">inserisci il titolo:</label>
-                            <input type="text" id="title" name="title" value={formData.title} placeholder="Inserisci il titolo del post" onChange={handleFormData} />
+                            <label htmlFor="title">Inserisci il titolo:</label>
+                            <input
+                                type="text"
+                                id="title"
+                                name="title"
+                                value={formData.title}
+                                placeholder="Inserisci il titolo del post"
+                                onChange={handleFormData}
+                            />
                         </div>
                         <div>
-                            <label htmlFor="content">inserisci il contenuto del post:</label>
-                            <input type="text" id="content" name="content" value={formData.content} placeholder="Descrizione del tuo post" onChange={handleFormData} />
+                            <label htmlFor="content">Inserisci il contenuto del post:</label>
+                            <input
+                                type="text"
+                                id="content"
+                                name="content"
+                                value={formData.content}
+                                placeholder="Descrizione del tuo post"
+                                onChange={handleFormData}
+                            />
                         </div>
                         <div>
-                            <label htmlFor="image">inserisci l'URL dell'immagine</label>
-                            <input type="text" id="image" name="image" value={formData.image} placeholder="URL" onChange={handleFormData} />
+                            <label htmlFor="image">Inserisci l'URL dell'immagine:</label>
+                            <input
+                                type="text"
+                                id="image"
+                                name="image"
+                                value={formData.image}
+                                placeholder="URL"
+                                onChange={handleFormData}
+                            />
                         </div>
                         <div>
-                            <label htmlFor="published">pubblicato</label>
-                            <input type="checkbox" checked={formData.published} id="published" name="published" onChange={handleFormData} />
+                            <label htmlFor="published">Pubblicato</label>
+                            <input
+                                type="checkbox"
+                                checked={formData.published}
+                                id="published"
+                                name="published"
+                                onChange={handleFormData}
+                            />
                         </div>
                         <div>
-                            <label htmlFor="html">HTML</label>
-                            <input type="checkbox" id="html" name="html" onChange={handleFormData} />
-                            <label htmlFor="css">CSS</label>
-                            <input type="checkbox" id="css" name="css" onChange={handleFormData} />
-                            <label htmlFor="js">Javascript</label>
-                            <input type="checkbox" id="js" name="js" onChange={handleFormData} />
-                            <label htmlFor="php">Php</label>
-                            <input type="checkbox" id="php" name="php" onChange={handleFormData} />
+                            {langTags.map((tag, index) => (
+                                <div key={tag}>
+                                    <label htmlFor={tag}>{tag}</label>
+                                    <input
+                                        type="checkbox"
+                                        id={tag}
+                                        data-index={index}
+                                        checked={formData.tags[index]}
+                                        name="tags"
+                                        onChange={handleFormData}
+                                    />
+                                </div>
+                            ))}
                         </div>
-                        <button className={style.btn_form} type="submit">AGGIUNGI POST</button>
+                        <button className={style.btn_form} type="submit">
+                            Aggiungi Post
+                        </button>
                     </form>
                 </div>
-                {postsArray.map(post => (
-                    post.published ?
+                {postsArray.map((post) => (
+                    post.published && (
                         <div className={style.col} key={post.id}>
-                            <Card title={post.title} image={post.image} content={post.content} tags={post.tags} />
-                            <button className={style.btn_form} onClick={() => deletePost(post.id)}>ELIMINA</button>
-                        </div> : null
-
+                            <Card
+                                title={post.title}
+                                image={post.image}
+                                content={post.content}
+                                tags={post.tags}
+                            />
+                            <button
+                                className={style.btn_form}
+                                onClick={() => deletePost(post.id)}
+                            >
+                                Elimina
+                            </button>
+                        </div>
+                    )
                 ))}
             </div>
         </div>
-    )
+    );
 }
